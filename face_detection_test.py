87fe41a5f5a4dc896fdb8c766b44741e17d81bac
alex <akalmykov@topcon.com>
2023-03-14 03:51:45 +0100

    face_detection_test.py: FPS added

--- face_detection_test.py
+++ face_detection_test.py
@@ -1,9 +1,12 @@
 import numpy as np
 import cv2
+import time
 faceCascade = cv2.CascadeClassifier('./data/haarcascade_frontalface_alt.xml')
 cap = cv2.VideoCapture(0)
 cap.set(3,640) # set Width
 cap.set(4,480) # set Height
+
+p_time = time.time()
 
 while True:
     ret, img = cap.read()
@@ -19,6 +22,11 @@ while True:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+    cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
     cv2.imshow('video',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27: # press 'ESC' to quit
