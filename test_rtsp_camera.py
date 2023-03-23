8302bcc898558a8083d978f072bd2cfcfaf2054b
alex <akalmykov@topcon.com>
2023-03-24 00:58:01 +0100

    test_rtsp_camera.py updated

--- test_rtsp_camera.py
+++ test_rtsp_camera.py
@@ -1,8 +1,6 @@
 import cv2
 import os
-
 RTSP_URL = 'rtsp://192.168.1.4:8554/unicast'
-
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
 
 cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
@@ -11,12 +9,13 @@ if not cap.isOpened():
     print('Cannot open RTSP stream')
     exit(-1)
 
-while True:
-    _, frame = cap.read()
-    cv2.imshow('RTSP stream', frame)
+try:
+    while True:
+        _, frame = cap.read()
+        cv2.imshow('RTSP stream', frame)
 
-    if cv2.waitKey(1) == 27:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+        if cv2.waitKey(1) == 27:
+            break
+finally:
+    cap.release()
+    cv2.destroyAllWindows()
